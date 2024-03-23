d22413f45a1587cc07ad2ee3f24365ca3e9cd01c
vanceaditya <aditya@vance.tech>
2024-03-23 12:14:10 +0530

    overflow hidden

--- components/SkillsAnimate.js
+++ components/SkillsAnimate.js
@@ -6,27 +6,29 @@ import { SiChakraui } from "react-icons/si";
 
 function SkillsAnimate() {
   return (
-    <div className='pDiv flex justify-center items-center '>
-          <div className='transDiv flex space-x-4 mx-auto '>
-            <FaHtml5 className='hover:scale-110 duration-300 '/>
-            <FaCss3 className='hover:scale-110 duration-300'/>
-            <IoLogoJavascript className='hover:scale-110 duration-300'/>
-            <FaNodeJs className='hover:scale-110 duration-300'/>
-            <FaJava className='hover:scale-110 duration-300'/>
-            <FaReact className='hover:scale-110 duration-300'/>
-            <TbBrandNextjs className='hover:scale-110 duration-300' />
-            <SiChakraui className='hover:scale-110 duration-300'/>
-          </div>
-          <div className='transDiv flex space-x-4 mx-auto '>
-            <FaHtml5 className='hover:scale-110 duration-300'/>
-            <FaCss3 className='hover:scale-110 duration-300'/>
-            <IoLogoJavascript className='hover:scale-110 duration-300'/>
-            <FaNodeJs className='hover:scale-110 duration-300'/>
-            <FaJava className='hover:scale-110 duration-300'/>
-            <FaReact className='hover:scale-110 duration-300'/>
-            <TbBrandNextjs className='hover:scale-110 duration-300' />
-            <SiChakraui className='hover:scale-110 duration-300'/>
-          </div>
+    <div className='overflow-hidden'>
+      <div className='pDiv flex justify-center items-center'>
+            <div className='transDiv flex space-x-4 mx-auto '>
+              <FaHtml5 className='hover:scale-110 duration-300 '/>
+              <FaCss3 className='hover:scale-110 duration-300'/>
+              <IoLogoJavascript className='hover:scale-110 duration-300'/>
+              <FaNodeJs className='hover:scale-110 duration-300'/>
+              <FaJava className='hover:scale-110 duration-300'/>
+              <FaReact className='hover:scale-110 duration-300'/>
+              <TbBrandNextjs className='hover:scale-110 duration-300' />
+              <SiChakraui className='hover:scale-110 duration-300'/>
+            </div>
+            <div className='transDiv flex space-x-4 mx-auto '>
+              <FaHtml5 className='hover:scale-110 duration-300'/>
+              <FaCss3 className='hover:scale-110 duration-300'/>
+              <IoLogoJavascript className='hover:scale-110 duration-300'/>
+              <FaNodeJs className='hover:scale-110 duration-300'/>
+              <FaJava className='hover:scale-110 duration-300'/>
+              <FaReact className='hover:scale-110 duration-300'/>
+              <TbBrandNextjs className='hover:scale-110 duration-300' />
+              <SiChakraui className='hover:scale-110 duration-300'/>
+            </div>
+      </div>
     </div>
   )
 }
