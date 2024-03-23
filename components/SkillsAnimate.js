248716effdbfad3b1e9621bc5bac4981e88ef54f
vanceaditya <aditya@vance.tech>
2024-03-23 13:33:57 +0530

    new sliding methods

--- components/SkillsAnimate.js
+++ components/SkillsAnimate.js
@@ -5,10 +5,27 @@ import { TbBrandNextjs } from "react-icons/tb";
 import { SiChakraui } from "react-icons/si";
 
 function SkillsAnimate({iconSize}) {
+  const tempArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   return (
     <div className=' overflow-x-hidden w-[80vw]'>
       <div className='pDiv flex items-center justify-center'>
-            <div className={`transDiv flex mx-auto`}>
+        {
+          tempArray.map((item, index) => {
+            return(
+              <div key={index} className={`transDiv flex mx-auto`}>
+                <FaHtml5 className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
+                <FaCss3 className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
+                <IoLogoJavascript className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
+                <FaNodeJs className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
+                <FaJava className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
+                <FaReact className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
+                <TbBrandNextjs className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`} />
+                <SiChakraui className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
+              </div>
+            )
+          })
+        }
+            {/* <div className={`transDiv flex mx-auto`}>
               <FaHtml5 className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
               <FaCss3 className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
               <IoLogoJavascript className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
@@ -17,17 +34,7 @@ function SkillsAnimate({iconSize}) {
               <FaReact className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
               <TbBrandNextjs className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`} />
               <SiChakraui className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
-            </div>
-            <div className={`transDiv flex mx-auto`}>
-              <FaHtml5 className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
-              <FaCss3 className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
-              <IoLogoJavascript className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
-              <FaNodeJs className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
-              <FaJava className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
-              <FaReact className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
-              <TbBrandNextjs className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`} />
-              <SiChakraui className={`hover:scale-110 duration-300 md:mx-2 mx-1 ${iconSize}`}/>
-            </div>
+            </div> */}
       </div>
     </div>
   )
